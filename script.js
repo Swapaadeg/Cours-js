1056898f122f4c77076dc7123b97112c63257f32
Marie Rivier <marie.rivier23@gmail.com>
2025-04-07 10:49:40 +0200

    les variables

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
-alert('Js fichier externe')
+// alert('Js fichier externe')
+
+/*const name = 'Kylian'
+// une variable doit commencer par une lettre, $ ou _ 
+let zipCode = 24000;
+let address = '1 rue Victor Hugo';
+let country = 'France';
+
+
+// console.log(name)
+// console.log(zipCode, address, country)
+// console.log(address)
+// console.log(country)
+*/
+
+// Chaînes de caractères
+
+const firstName = 'John'
+const lastName = 'Doe'
+
+console.log('J\'aime le Javascript')
+
+console.log('Bonjour ' + firstName + ' ' + lastName)
+console.log(`Bonjour ${firstName} ${lastName}`)
+
+// Type number 
+
+let number1 = 5
+let number2 = 3
+
+// console.log(typeof number1)
+// console.log(typeof number2)
+
+// const result = number1 + number2
+// number1 +=4 //donne 6
+// console.log(number1)
+
+console.log(number1 % 2)
+// indique le reste de la division 5/2 = 2+2 reste 1
+
+//Autres types de variables
+
+// Chaîne de caractères 
+const texte = 'chaîne de caractères'
+
+//number
+const number = 5
+
+//boolean
+const booleanValue = false 
+
+//null
+const nullValue = null
+
+//undefined
+let undefinedValue = undefined;
+
+// Tableaux, objets, fonctions...
